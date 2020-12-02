57544ab26149aa9c1d5776433e13e1ea55b5a59a
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-12-02 09:16:45 -0500

    first commit

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const validateEmail = (email) => re.test(email);
+
+const UserSchema = new Schema(
+    {
+        username: { 
+            type: String, 
+            required: [true, 'Username required'],
+            maxlength: 30,
+            trim: true, 
+        },
+        email: { 
+            type: email, 
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [re, 'Please fill a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        membership: String,
+        firstName: {
+            type: String,
+            required: true,
+            maxlength: 50,
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            maxlength: 50,
+            trim: true,
+        }
+    }
+)
+
+// Virtual for user url
+UserSchema
+.virtual('url')
+.get(() => '/users/user/' + this._id);
+
+// Virtual for user fullname
+UserSchema
+.virtual('fullname')
+.get(() => this.firstName + ' ' + this.lastName);
+
+
+module.exports = mongoose.model('User', UserSchema);
