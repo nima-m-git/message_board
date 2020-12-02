ceeb575c590b6c1961476c2b2de0fdbdf70bf46a
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-12-02 18:11:11 -0500

    custom validators added for email and username, checking if already existing in db

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,15 +13,30 @@ exports.user_create_get = (req, res) => {
 exports.user_create_post = [
     body('firstName', 'First name must be specified').trim().isLength({ min: 1}).escape(),
     body('lastName', 'Last name must be specified').trim().isLength({ min: 1}).escape(),
-    body('email', 'Must be valid email').isEmail(),
+    body('email', 'Must be valid email').isEmail()
+        .custom(async (value, { req }) => {
+            const emails = (await User.find({}, 'email')).map(obj => obj.email);
+            if (emails.includes(value)) {
+                throw new Error('Email already registered');
+            }
+            return true;
+        }),
+    body('username', 'Username must be specified').trim().isLength({ min: 1}).escape()
+        .custom(async (value, { req }) => {
+            const usernames = (await User.find({}, 'username')).map(obj => obj.username);
+            if (usernames.includes(value)) {
+                throw new Error('Username already exists');
+            }
+            return true;
+        }),
     body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'),
-    body('confirm-password').custom((value, { req }) => {
-        console.log({ value, ...req.body, })
-        if (value !== req.body.password) {
-            throw new Error('Passwords must match');
-        }
-        return true;
-    }),
+    body('confirm-password')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords must match');
+            }
+            return true;
+        }),
 
     (req, res, next) => {
         const errors = validationResult(req);
